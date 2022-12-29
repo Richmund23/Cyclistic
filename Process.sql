6a41be6a799b9a79f610c3b27daa621704d52a52
Rich23 <97440488+Richmund23@users.noreply.github.com>
2022-12-29 16:02:07 +0800

    Update Process.sql

--- Process.sql
+++ Process.sql
@@ -112,5 +112,50 @@ CYCLISTIC BIKE-SHARE ANALYSIS
 	WHERE row_num > 1;
 	
 	
-	-- 
+	-- Check for blank entries
+	-- [column name]: ALL columns
 	
+	FROM
+		tripdata
+	WHERE
+		[column name] IS NULL;
+	
+	
+	-- Delete blank entries
+	
+	DELETE
+	FROM
+		tripdata
+	WHERE
+		start_station_name IS NULL;
+		
+		
+	-- Added the quarter_name column temporarily shown as month
+	-- Added the month_name column which contains the month the ride trips took place in mmm format
+	-- Updated the ride_length column into ride_length_minutes column which contains the length of the ride trips in minutes
+	-- Renamed member_casual column as rider_type
+
+	
+	SELECT
+		ride_id,
+		rideable_type,
+		started_at,
+		ended_at,
+		ROUND(DATEDIFF(SECOND,started_at,ended_at)/60.0, 2, 2) AS ride_length_minutes,
+		CAST(DATENAME(m, started_at) AS nvarchar(3)) AS quarter_name,
+		CAST(DATENAME(m, started_at) AS nvarchar(3)) AS month_name,
+		day_of_week,
+		DATENAME(WEEKDAY, started_at) AS day_name,
+		start_station_name,
+		start_station_id,
+		end_station_name,
+		end_station_id,
+		start_lat,
+		start_lng,
+		end_lat,
+		end_lng,
+		member_casual AS rider_type
+	FROM
+		tripdata
+	
+	-- Proceed to next step: Analyze
