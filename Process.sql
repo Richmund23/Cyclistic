2ede1b0f458f56bbb14d2208288a3ed347087b5a
Rich23 <97440488+Richmund23@users.noreply.github.com>
2022-12-29 14:08:48 +0800

    Update Process.sql

--- Process.sql
+++ Process.sql
@@ -4,79 +4,81 @@ CYCLISTIC BIKE-SHARE ANALYSIS
 */
 
 -- PROCESS
-  -- Data Cleaning
+	-- Data Cleaning
+	-- Combining all tables into a single table
   
-  -- Combining all tables into a single table
+	SELECT *
+		INTO
+			tripdata
+		FROM
+		(
+		SELECT
+		*
+		FROM
+			cyclistic.dbo.Jan$
+		UNION ALL
+		SELECT
+		*
+		FROM
+			cyclistic.dbo.Feb$
+		UNION ALL
+		SELECT
+		*
+		FROM
+			cyclistic.dbo.Mar$
+		UNION ALL
+		SELECT
+		*
+		FROM
+			cyclistic.dbo.Apr$
+		UNION ALL
+		SELECT
+		*
+		FROM
+			cyclistic.dbo.May$
+		UNION ALL
+		SELECT
+		*
+		FROM
+			cyclistic.dbo.Jun$
+		UNION ALL
+		SELECT
+		*
+		FROM
+			cyclistic.dbo.Jul$
+		UNION ALL
+		SELECT
+		*
+		FROM
+			cyclistic.dbo.Aug$
+		UNION ALL
+		SELECT
+		*
+		FROM
+			cyclistic.dbo.Sep$
+		UNION ALL
+		SELECT
+		*
+		FROM
+			cyclistic.dbo.Oct$
+		UNION ALL
+		SELECT
+		*
+		FROM
+			cyclistic.dbo.Nov$
+		UNION ALL
+		SELECT
+		*
+		FROM
+			cyclistic.dbo.Dec$
+		) a
+		
+	-- Check for misspellings and irregularities
+	-- [column string]: rideable_type, start_station_name, start_station_id, end_station_name, end_station_id, member_casual
   
-SELECT *
-	INTO
+	SELECT DISTINCT 
+		[column string]
+	FROM
 		tripdata
-	FROM
-	(
-	SELECT
-	*
-	FROM
-		cyclistic.dbo.Jan$
-	UNION ALL
-	SELECT
-	*
-	FROM
-		cyclistic.dbo.Feb$
-	UNION ALL
-	SELECT
-	*
-	FROM
-		cyclistic.dbo.Mar$
-	UNION ALL
-	SELECT
-	*
-	FROM
-		cyclistic.dbo.Apr$
-	UNION ALL
-	SELECT
-	*
-	FROM
-		cyclistic.dbo.May$
-
-	UNION ALL
-	SELECT
-	*
-	FROM
-		cyclistic.dbo.Jun$
-
-	UNION ALL
-	SELECT
-	*
-	FROM
-		cyclistic.dbo.Jul$
-	UNION ALL
-	SELECT
-	*
-	FROM
-		cyclistic.dbo.Aug$
-
-	UNION ALL
-	SELECT
-	*
-	FROM
-		cyclistic.dbo.Sep$
-	UNION ALL
-	SELECT
-	*
-	FROM
-		cyclistic.dbo.Oct$
-	UNION ALL
-	SELECT
-	*
-	FROM
-		cyclistic.dbo.Nov$
-	UNION ALL
-	SELECT
-	*
-	FROM
-		cyclistic.dbo.Dec$
-	) a
-  
-  -- Check for misspellings and irregularities
-  -- [column string]: rideable_type, start_station_name, start_station_id, end_station_name, end_station_id, member_casual
-  
+		
+	--
