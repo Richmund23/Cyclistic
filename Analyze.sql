27a3647e8727584e56093d3f3cb6125fbea0fce4
Rich23 <97440488+Richmund23@users.noreply.github.com>
2022-12-29 21:52:12 +0800

    Update Analyze.sql

--- Analyze.sql
+++ Analyze.sql
@@ -87,7 +87,7 @@ CYCLISTIC BIKE-SHARE ANALYSIS
 
 
 	-- Ride Length
-	-- Explore Ride Length by rider_type
+	-- Explore Ride Length by rider type
 	
 	SELECT
 		rider_type,
@@ -103,7 +103,7 @@ CYCLISTIC BIKE-SHARE ANALYSIS
 		
 		
 	-- Ride Length
-	-- Explore Ride Length by bike_type
+	-- Explore Ride Length by bike type
 	
 	SELECT
 		bike_type,
@@ -116,3 +116,138 @@ CYCLISTIC BIKE-SHARE ANALYSIS
 		analyzetripdata
 	GROUP BY
 		bike_type
+		
+		
+	-- Explore Ride Length by quarter
+	-- [rider type] : 'member' or 'casual'
+	
+	SELECT 
+		rider_type,
+		quarter_name,
+		AVG(ride_length_minutes) AS avg_ride_length
+	INTO
+		ridelength_by_quarter_[rider type]
+	FROM
+		analyzetripdata
+	WHERE
+		rider_type = '[rider type]'
+	GROUP BY
+		rider_type,
+		quarter_name
+	ORDER BY
+		quarter_name
+		
+	
+	-- Explore Ride Length by month
+	-- '[rider type]' : 'member' or 'casual'
+	SELECT 
+		month_name,
+		AVG(ride_length_minutes) AS avg_ride_length_[rider type]
+	INTO
+		ridelength_by_month_[rider type]
+	FROM
+		analyzetripdata
+	WHERE
+		rider_type = '[rider type]'
+	GROUP BY
+		rider_type,
+		month_name
+	ORDER BY
+		month_name
+	
+		
+	-- Explore Ride Length by day
+	-- '[rider type]' : 'member' or 'casual'
+	
+	SELECT 
+		day_of_week,
+		day_name,
+		AVG(ride_length_minutes) AS avg_ride_length_[rider type]
+	INTO
+		ridelength_by_day_[rider type]
+	FROM
+		analyzetripdata
+	WHERE
+		rider_type = '[rider type]'
+	GROUP BY
+		rider_type,
+		day_of_week,
+		day_name
+	ORDER BY
+		day_of_week
+		
+	
+	-- Explore Number of Rides
+	-- '[rider type]' : 'member' or 'casual'
+		
+	
+	SELECT
+		rider_type,
+	COUNT(*) AS ride_trips
+	INTO
+		ridetrips_by_ridertype
+	FROM
+		analyzetripdata
+	GROUP BY
+		rider_type
+		
+		
+	-- Explore Number of Rides by quarter
+	-- '[rider type]' : 'member' or 'casual'
+
+	SELECT
+		rider_type,
+		quarter_name,		
+		COUNT(*) AS ride_trips
+	INTO
+		ridetrips_by_quarter_[rider type]
+	FROM
+		analyzetripdata
+	WHERE
+		rider_type = '[rider type]'
+	GROUP BY
+		rider_type,
+		quarter_name
+	ORDER BY
+		quarter_name
+		
+	-- Explore Number of Rides by month
+	-- '[rider type]' : 'member' or 'casual'
+
+	SELECT
+		rider_type,
+		month_name,		
+		COUNT(*) AS ride_trips
+	INTO
+		ridetrips_by_month_[rider type]
+	FROM
+		analyzetripdata
+	WHERE
+		rider_type = '[rider type]'
+	GROUP BY
+		rider_type,
+		month_name
+	ORDER BY
+		month_name
+		
+
+	-- Explore Number of Rides by day
+	-- '[rider type]' : 'member' or 'casual'
+	
+	SELECT
+		rider_type,
+		month_name,		
+		COUNT(*) AS ride_trips
+	INTO
+		ridetrips_by_month_[rider type]
+	FROM
+		analyzetripdata
+	WHERE
+		rider_type = '[rider type]'
+	GROUP BY
+		rider_type,
+		month_name
+	ORDER BY
+		month_name
+	
+	
